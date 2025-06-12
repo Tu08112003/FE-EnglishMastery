231171f63ac84fa58c8c4e6bfed5202e86455e82
Tu08112003 <huynhtu012023@gmail.com>
2025-06-12 23:02:34 +0700

    feat(auth): implement Login with Google feature

--- src/service/authService.js
+++ src/service/authService.js
@@ -1,65 +1,81 @@
-import axios from '../utils/axios-customize'
+import axios from "../utils/axios-customize";
 
-const authLogin = ({email, password}) => {
-    const URL_BACKEND = '/auth-service/login';
-    const data = {
-        email: email,
-        password: password
-    };
-    return axios.post(URL_BACKEND,data);
-}
-
-const authRegister = ({userName, email, password}) => {
-    const URL_BACKEND = '/auth-service/register';
-    const data = {
-        userName: userName,
-        email: email,
-        password: password
-    };
-    return axios.post(URL_BACKEND,data);
-}
-const authLogout = ({accessToken,refreshToken}) => {
-    const URL_BACKEND = '/auth-service/logout';
-    const data = {
-        accessToken: accessToken,
-        refreshToken: refreshToken
-    }
-    return axios.post(URL_BACKEND, data);
+const authLogin = ({ email, password }) => {
+  const URL_BACKEND = "/auth-service/login";
+  const data = {
+    email: email,
+    password: password,
+  };
+  return axios.post(URL_BACKEND, data);
 };
 
-const checkRefreshToken = ({refreshToken}) => {
-    const URL_BACKEND = '/auth-service/refresh';
-    const data = {
-        refreshToken: refreshToken
-    }
-    return axios.post(URL_BACKEND, data);
-}
+const authRegister = ({ userName, email, password }) => {
+  const URL_BACKEND = "/auth-service/register";
+  const data = {
+    userName: userName,
+    email: email,
+    password: password,
+  };
+  return axios.post(URL_BACKEND, data);
+};
+const authLogout = ({ accessToken, refreshToken }) => {
+  const URL_BACKEND = "/auth-service/logout";
+  const data = {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-const checkEmail = ({email}) => {
-    const URL_BACKEND = '/auth-service/checkEmail';
-    const data = {
-        email: email
-    }
-    return axios.post(URL_BACKEND, data);
-}
+const checkRefreshToken = ({ refreshToken }) => {
+  const URL_BACKEND = "/auth-service/refresh";
+  const data = {
+    refreshToken: refreshToken,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-const checkOTP = ({email, otpCode}) =>{
-    const URL_BACKEND = '/auth-service/checkOTP';
-    const data = {
-        email: email,
-        otpCode: otpCode
-    };
-    return axios.post(URL_BACKEND,data);
-}
+const checkEmail = ({ email }) => {
+  const URL_BACKEND = "/auth-service/checkEmail";
+  const data = {
+    email: email,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-const resetPassword = ({email, newPassword}) =>{
-    const URL_BACKEND = '/auth-service/resetPassword';
-    const data = {
-        email: email,
-        newPassword: newPassword
-    }
-    return axios.post(URL_BACKEND, data);
+const checkOTP = ({ email, otpCode }) => {
+  const URL_BACKEND = "/auth-service/checkOTP";
+  const data = {
+    email: email,
+    otpCode: otpCode,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-}
+const resetPassword = ({ email, newPassword }) => {
+  const URL_BACKEND = "/auth-service/resetPassword";
+  const data = {
+    email: email,
+    newPassword: newPassword,
+  };
+  return axios.post(URL_BACKEND, data);
+};
 
-export { authLogin, authRegister, authLogout, checkRefreshToken, checkEmail, checkOTP, resetPassword};
+const authGoogleLogin = ({ idToken }) => {
+  const URL_BACKEND = "/auth-service/google";
+  const data = { 
+    idToken: idToken 
+  };
+  return axios.post(URL_BACKEND, data);
+};
+
+export {
+  authLogin,
+  authRegister,
+  authLogout,
+  checkRefreshToken,
+  checkEmail,
+  checkOTP,
+  resetPassword,
+  authGoogleLogin,
+};
