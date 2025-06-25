a608f7f6d1133ea8ef4dbd083a03b772cb6fede6
Tu08112003 <huynhtu012023@gmail.com>
2025-06-25 20:53:00 +0700

    refactor(exam): rename file convert time

--- src/utils/convertFormatTime.js
+++ src/utils/convertFormatTime.js
@@ -1,5 +1,5 @@
-const parseFormatTime = (timeStr) => {
+const convertFormatTime = (timeStr) => {
   const [hrs, mins, secs] = timeStr.split(":").map(Number);
   return hrs + mins / 60 + secs / 3600;
 };
-export default parseFormatTime;
+export default convertFormatTime;
